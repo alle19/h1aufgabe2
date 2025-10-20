8a01fc7f4886fb0c63645c55506940ae60a89d95
alle19 <allexandra.bota@gmail.com>
2025-10-20 10:38:28 +0300

    methode1

---
+++ src/ArrayOperationen.java
@@ -0,0 +1,13 @@
+public class ArrayOperationen {
+
+    public static int findMax(int[] numbers) {
+        int max = numbers[0];
+        for (int num : numbers) {
+            if (num > max) {
+                max = num;
+            }
+        }
+        return max;
+
+    }
+}
